c1dd39993801d3d004c22bd961df677b4985fe0c
ricidkumbara <ricidkumbara@gmail.com>
2024-06-30 11:20:05 +0700

    update

--- main.mjs
+++ main.mjs
@@ -1,10 +1,10 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-    console.log('request accepted...');
+    console.log('Request accepted...');
     
     res.write(JSON.stringify({code: 200}));
     res.end();
 })
 
-server.listen(3000);
+server.listen(3001);
